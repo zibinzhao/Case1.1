ed8092e7ee8478188f5c1ee96e5e0dd47b4c7742
zibinzhao <48482836+zibinzhao@users.noreply.github.com>
2024-11-07 17:12:23 +0000

    Update app_mix_initiative.py

--- app_mix_initiative.py
+++ app_mix_initiative.py
@@ -158,9 +158,8 @@ st.sidebar.header("3. Choose model")
 classifier = st.sidebar.selectbox("Model", ("Random Forest Regression", "Random Forest classification"))
 
 ###############Visualising SHAP Explantions###############
-@st.cache_data(persist="disk")
-def st_shap(_plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{_plot.html()}</body>"
+def st_shap(plot, height=None):
+    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
 
 ###############Training Random Forest classifier Hyperparameters###############
